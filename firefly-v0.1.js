2fe9ae1c2fe7776bb92e643b1c7261da039bdaf8
Christina Zhu <cszhu@ucdavis.edu>
2016-01-04 00:09:20 -0800

    version 0.1 yay :)

---
+++ firefly-v0.1.js
@@ -0,0 +1,103 @@
+function firefly(id, num, sz, color) {
+  createCanvas(id);
+  draw(num, sz, color);
+}
+
+function createCanvas(id) {
+  var canvas = document.createElement('canvas');
+  div = document.getElementById(id);
+  console.log(id);
+  console.log(div);
+  canvas.id = "fireflyLayer";
+  canvas.height = div.offsetHeight;
+  canvas.width = div.offsetWidth;
+  canvas.style.position = "absolute";
+  canvas.style.zIndex = 100;
+  canvas.style.left = "0";
+  canvas.style.top = "0";
+  div.appendChild(canvas);
+}
+
+function draw(num, sz, color) {
+  var mainCanvas = document.getElementById("fireflyLayer");
+  var mainContext = mainCanvas.getContext('2d');
+  var circles = [];
+
+  var requestAnimationFrame = window.requestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.msRequestAnimationFrame;
+
+  function Circle(radius, speed, width, xPos, yPos) {
+    this.radius = radius;
+    this.speed = speed;
+    this.width = width;
+    this.xPos = xPos;
+    this.yPos = yPos;
+    this.opacity = .05 + Math.random() * .8;
+
+    this.counter = 0;
+
+    var signHelper = Math.floor(Math.random() * 2);
+
+    if (signHelper == 1) {
+      this.sign = -1;
+    } else {
+      this.sign = 1;
+    }
+  }
+
+  Circle.prototype.update = function() {
+    this.counter += this.sign * this.speed;
+
+    mainContext.beginPath();
+
+    mainContext.arc(this.xPos + Math.cos(this.counter / 700) * this.radius,
+      this.yPos + Math.sin(this.counter / 700) * this.radius,
+      this.width,
+      0,
+      Math.PI * 2,
+      false);
+
+    mainContext.closePath();
+
+    var hex = color.replace('#','');
+    var r = parseInt(hex.substring(0,2), 16);
+    var g = parseInt(hex.substring(2,4), 16);
+    var b = parseInt(hex.substring(4,6), 16);
+    mainContext.fillStyle = 'rgba(0, 0, 0, ' + this.opacity + ')';
+    // mainContext.fillStyle = 'rgba(' + r + ', ' + g + ', ' + b + ', ' + this.opacity + ')';
+    mainContext.fill();
+  };
+
+  var szNum;
+  if (sz == "big") { szNum = 10; }
+    else if (sz == "medium") { szNum = 5; }
+    else if (sz == "small") { szNum = 3; }
+    else if (sz == "tiny") { szNum = 1; }
+    else { szNum = 5; }
+
+  function drawCircles() {
+    for (var i = 0; i < num; i++) {
+      var randomX = Math.round(-200 + Math.random() * 700);
+      var randomY = Math.round(-200 + Math.random() * 700);
+      var speed = .2 + Math.random() * 3;
+      var size = Math.random() * szNum;
+
+      var circle = new Circle(100, speed, size, randomX, randomY);
+      circles.push(circle);
+    }
+    draw();
+  }
+  drawCircles();
+
+  function draw() {
+    mainContext.clearRect(0, 0, 500, 500);
+
+    for (var i = 0; i < circles.length; i++) {
+      var myCircle = circles[i];
+      myCircle.update();
+    }
+    requestAnimationFrame(draw);
+  }
+};
